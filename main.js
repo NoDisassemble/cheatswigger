e9753433fe764f3938051fee30ad0a74882f0cd0
NoDisassemble <anything@dant35an0nym0u5.com>
2021-11-13 14:39:09 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -8,34 +8,6 @@ function initialLoad() {
 
 initialLoad();
 
-// Accordion 
-const acc_btns = document.querySelectorAll(".accordion-header");
-const acc_contents = document.querySelectorAll(".accordion-body");
-
-acc_btns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        acc_contents.forEach((acc) => {
-            if (
-                e.target.nextElementSibling !== acc &&
-                acc.classList.contains("active")
-            ) {
-                acc.classList.remove("active");
-                acc_btns.forEach((btn) => btn.classList.remove("active"));
-            }
-        });
-
-        const panel = btn.nextElementSibling;
-        panel.classList.toggle("active");
-        btn.classList.toggle("active");
-    });
-});
-
-window.onclick = (e) => {
-    if (!e.target.matches(".accordion-header")) {
-        acc_btns.forEach((btn) => btn.classList.remove("active"));
-        acc_contents.forEach((acc) => acc.classList.remove("active"));
-    }
-};
 
 // Load external HTML files
 function loadHtml(id, filename) {
